6e0ed52b1663375cedeff551c31f7d95ff59879d
laureca <anynnha.laureca@gmail.com>
2020-01-13 15:47:42 -0300

    adicionado validação nas idades, sexo e também representação com imagens dinâmicas

--- aula12.ex/ex015/script.js
+++ aula12.ex/ex015/script.js
@@ -7,6 +7,48 @@ function verificar() {
     if(fAno.value.length == 0 || fAno.value > ano) {
         window.alert('[ERRO] Verifique os dados e tente novamente!')
     } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fAno.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
 
+        if(fsex[0].checked){
+            genero = 'homem'
+        } else {
+            genero = 'mulher'
+        }
+
+        if(idade < 15){
+            if(genero == 'homem'){
+                img.setAttribute('src', 'menino.png')
+            } else {
+                img.setAttribute('src', 'menina.png')
+            }
+        } else if(idade < 25) {
+            if(genero == 'homem'){
+                img.setAttribute('src', 'cara.png')
+            } else {
+                img.setAttribute('src', 'mina.png')
+            }
+        } else if(idade < 50) {
+            if(genero == 'homem'){
+                img.setAttribute('src', 'homem.png')
+            } else {
+                img.setAttribute('src', 'mulher.png')
+            }
+        } else if(idade < 110) {
+            if(genero == 'homem'){
+                img.setAttribute('src', 'senhor.png')
+            } else {
+                img.setAttribute('src', 'senhora.png')
+            }
+        } else {
+            res.innerHTML = `Provavelmente essa pessoa já veio a óbito.`
+        }
+
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
     }
 }

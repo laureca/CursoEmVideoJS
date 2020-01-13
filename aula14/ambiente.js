f89834cee1e1994800c34a5c8cd34eae96205c4e
laureca <anynnha.laureca@gmail.com>
2020-01-13 17:09:13 -0300

    testando exercícos de repetição e depuração

---
+++ aula14/ambiente.js
@@ -0,0 +1,19 @@
+/*var i = 0
+
+while(c < 5){
+    console.log(i)
+    i++
+}*/
+
+/*var i = 0
+
+do{
+    console.log(i)
+    i++
+}while(c < 5)*/
+
+console.log('Vai começar!')
+for (i = 0; i < 5; i++) {
+    console.log(i)
+}
+console.log('FIM!')

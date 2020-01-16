c1b1848988ebcd764060f9fb0ef9bdf0e6cd0da1
laureca <anynnha.laureca@gmail.com>
2020-01-16 15:12:24 -0300

    testando variáveis compostas

---
+++ aula15/ambiente.js
@@ -0,0 +1,12 @@
+var num = [5,3,7,1,9]
+
+// console.log(num)
+// console.log(num.length)
+// num.push(2)
+// console.log(num)
+// num.sort()
+// console.log(num)
+
+for(var i = 0; i < num.length; i++){
+    console.log(num[i])
+}
---
+++ aula15/vetorNaTela.js
@@ -0,0 +1,9 @@
+var valores = [3,7,8,6,4,1,0,9,2,5]
+
+// for(var i = 0; i < valores.length; i++){
+//     console.log(valores[i])
+// }
+
+for(var i in valores){
+    console.log(valores[i])
+}
